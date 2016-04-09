96ef92667dc6ee143c80f526e5a0d25954052840
iWECon <i.1214@yahoo.com>
2016-04-09 22:14:43 +0800

    Initial Commit

---
+++ SliderNavigation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SliderNavigation
+//
+//  Created by iWe on 16/4/9.
+//  Copyright © 2016年 iWe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
